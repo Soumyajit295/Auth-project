fea3064dd6faacb154c5443f6f9d195d5484d7bc
Soumyajit de <soumyajit.de20190@gmail.com>
2024-09-12 12:49:10 +0530

    Fourth commit

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json({limit : '50mb'}))
 app.use(express.urlencoded({extended : true,limit : '50mb'}))
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://auth-project-frontend-k9wa.onrender.com',
     credentials: true
 }));
 
